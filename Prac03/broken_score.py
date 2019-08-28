3f04f0ae3cf922f4d77713f091234268764403e9
Chitra <jonathan.keerikattil@my.jcu.edu.au>
2019-08-28 22:30:01 +1000

    Refactored to show functions

--- Prac03/broken_score.py
+++ Prac03/broken_score.py
@@ -1,11 +1,20 @@
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif 50 <= score:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    result = ''
+    score = float(input("Enter score: "))
+    result = check_score(score)
+    print(result)
+
+def check_score(score):
+    if score < 0:
+        return 'invalid score'
+    elif score > 100:
+        return 'invalid score'
+    elif score >= 90:
+        return 'excellent'
+    elif 50 <= score:
+        return 'passable'
+    else:
+        return 'bad'
+
+
+main()

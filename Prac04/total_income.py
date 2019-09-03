0a5b36c1e158388d67d1f4b6f46eec54186d59e1
Chitra <jonathan.keerikattil@my.jcu.edu.au>
2019-09-03 20:19:06 +1000

    Calculate a list of cumulative totals

---
+++ Prac04/total_income.py
@@ -0,0 +1,40 @@
+# def main():
+#     incomes = []
+#     months = 0
+#     number_of_months = input("How many months: ")
+#     while months < int(number_of_months):
+#         income = input("Enter income for month {}: ".format(months+1))
+#         incomes.append(income)
+#         months = months + 1
+#     print(incomes)
+# 
+# 
+# main()
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month {}: ".format(str(month))))
+        incomes.append(income)
+
+    cumulative_income(incomes, number_of_months)
+
+
+def cumulative_income(incomes, number_of_months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()

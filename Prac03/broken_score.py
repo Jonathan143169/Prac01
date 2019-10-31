9adf19f195f212741c6d6f608784019b4fc330d0
Chitra <jonathan.keerikattil@my.jcu.edu.au>
2019-10-31 10:58:11 +1000

    Initial commit

--- Prac03/broken_score.py
+++ Prac03/broken_score.py
@@ -1,20 +1,23 @@
-def main():
-    result = ''
-    score = float(input("Enter score: "))
-    result = check_score(score)
-    print(result)
+# def main():
+#     score = float(input("Enter score: "))
+#     result = check_score(score)
+#     print(result)
+#
+#
+# def check_score(score):
+#     if score < 0:
+#         return 'invalid score'
+#     elif score > 100:
+#         return 'invalid score'
+#     elif score >= 90:
+#         return 'excellent'
+#     elif 50 <= score:
+#         return 'passable'
+#     else:
+#         return 'bad'
+#
+#
+# main()
+words = ["aye", "bee", "sea"]
+print(" / ".join(words))
 
-def check_score(score):
-    if score < 0:
-        return 'invalid score'
-    elif score > 100:
-        return 'invalid score'
-    elif score >= 90:
-        return 'excellent'
-    elif 50 <= score:
-        return 'passable'
-    else:
-        return 'bad'
-
-
-main()
